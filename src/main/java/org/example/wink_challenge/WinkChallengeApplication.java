20e0b6713d34b96624febcdd53bacd45b45204b2
Tema <sezemov2@gmail.com>
2025-10-25 19:36:02 +0300

    Initial commit

---
+++ src/main/java/org/example/wink_challenge/WinkChallengeApplication.java
@@ -0,0 +1,13 @@
+package org.example.wink_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinkChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinkChallengeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/wink_challenge/controllers/AuthController.java
@@ -0,0 +1,13 @@
+package org.example.wink_challenge.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(path = "/auth", method = RequestMethod.GET)
+public class AuthController {
+
+}
