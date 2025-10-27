c3b0984868b560c22773d2e2da8887fbe9c5bb42
Tema <sezemov2@gmail.com>
2025-10-27 13:56:42 +0300

    renamed folder repository_interfaces to repositories and marked TaskRepository as @Repository

--- backend/src/main/java/org/example/wink_challenge/repositories/TaskRepository.java
+++ backend/src/main/java/org/example/wink_challenge/repositories/TaskRepository.java
@@ -3,6 +3,7 @@ package org.example.wink_challenge.repository_interfaces;
 import org.example.wink_challenge.entities.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
     TaskEntity findByName(String name);
 }
